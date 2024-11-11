86a0dfb44dba8ad2e02fb0283a084d16a9a6b276
Toast <bored@toasted.me>
2024-11-11 18:35:12 +0400

    NewBegining

---
+++ demo/script.js
@@ -0,0 +1,16 @@
+console.log('ЫЫЫЫЫЫЫЫЫ');
+
+let m = "SSSSSS"
+
+console.log(m);
+
+let g = 'ы'
+let a = 'п'
+
+let message = `${g} рееееееееее ${a}`
+
+console.log(message)
+
+console.log(8n + 9n)
+
+console.log('a'==11)
