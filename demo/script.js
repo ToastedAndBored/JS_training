df9f2f6ae272284a405f1993e7ba29af2b400fc3
Toast <bored@toasted.me>
2024-11-14 01:19:40 +0400

    Add FunctionsAndObj

--- demo/script.js
+++ demo/script.js
@@ -14,3 +14,79 @@ console.log(message)
 console.log(8n + 9n)
 
 console.log('a'==11)
+
+let count = 10
+
+//console.log(count+=1)
+
+//if (m>g) {
+//  console.log("ыыыыы")
+//} else {
+//  console.log('аааааа')
+//}
+
+//alert('АААААА')
+
+// let user = prompt('скажи FFFFFF', 'FFFFFF')
+
+// if (user === 'FFFFFF') {
+//   console.log('ЕЕЕЕЕЕЕЕЕ')
+// } else {
+//   console.log('УЭЭЭЭЭЭЭЭЭ')
+// }
+
+// let age = +prompt('Сколько?')
+
+// switch (true) {
+//   case age == 0:{
+//     console.log('тебя не существует')
+//     break
+//   }
+//   case (age>=1)&&(age<=17):{
+//     console.log("ты существо мелкое")
+//     break
+//   }
+//   case (age>=18)&&(age<=100):{
+//     console.log('ты существо живое')
+//     break
+//   }
+//   case age>100:{
+//     console.log('ты мертво')
+//     break
+//   }
+//   default:{
+//     console.log('УЭЭЭЭЭЭЭЭ')
+//   }
+// }
+while (count<=20) {
+  console.log(count)
+  count++
+}
+
+const demo = () => {
+    
+}
+
+let aa = true
+
+const mes = aa === true
+  ? 'ы'
+  : 's'
+
+console.log(mes)
+
+const user = {
+  age: 28,
+  name: 'Alex'
+}
+
+console.log(user.name)
+
+const prom = prompt('ы')
+const pormm = prompt(`sdafas ${prom}`)
+
+const obj = {
+  [prom]:pormm
+}
+
+console.log(obj)
